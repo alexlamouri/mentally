60bf49cb8ce0191e33c4b8824e5c452a4bd24c7c
Kavya Mehta <kavyamehta@Kavyas-MacBook-Pro.local>
2020-02-14 17:05:13 -0500

    modified profile activity

--- MentAlly/app/src/main/java/com/pacman/MentAlly/ui.profile/ProfileActivity.java
+++ MentAlly/app/src/main/java/com/pacman/MentAlly/ui.profile/ProfileActivity.java
@@ -1,4 +1,4 @@
-package com.pacman.MentAlly.profile;
+package com.pacman.MentAlly.ui.profile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +9,22 @@ import android.widget.TextView;
 import com.pacman.MentAlly.R;
 
 public class ProfileActivity extends AppCompatActivity {
+    Button profile_btn;
+    Button first_name_btn;
+    Button last_name_btn;
+    Button email_btn;
+    Button dob_btn;
+    Button country_btn;
+    Button password_btn;
+
+    TextView first_name_txt;
+    TextView last_name_txt;
+    TextView email_txt;
+    TextView country_txt;
+    TextView dob_txt;
+    TextView password_txt;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,33 +32,33 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         //set all button transparency levels
-        Button profile_btn = findViewById(R.id.change_profile_pic);
+        profile_btn = findViewById(R.id.change_profile_pic);
         profile_btn.getBackground().setAlpha(65);
-        Button first_name_btn = findViewById(R.id.edit_first_name);
+        first_name_btn = findViewById(R.id.edit_first_name);
         first_name_btn.getBackground().setAlpha(65);
-        Button last_name_btn = findViewById(R.id.edit_last_name);
+        last_name_btn = findViewById(R.id.edit_last_name);
         last_name_btn.getBackground().setAlpha(65);
-        Button email_btn = findViewById(R.id.edit_email);
+        email_btn = findViewById(R.id.edit_email);
         email_btn.getBackground().setAlpha(65);
-        Button dob_btn = findViewById(R.id.edit_dob);
+        dob_btn = findViewById(R.id.edit_dob);
         dob_btn.getBackground().setAlpha(65);
-        Button country_btn = findViewById(R.id.edit_country);
+        country_btn = findViewById(R.id.edit_country);
         country_btn.getBackground().setAlpha(65);
-        Button password_btn = findViewById(R.id.edit_password);
+        password_btn = findViewById(R.id.edit_password);
         password_btn.getBackground().setAlpha(65);
 
         //set all textview transparency level
-        TextView first_name_txt = findViewById(R.id.firstname_textview);
+        first_name_txt = findViewById(R.id.firstname_textview);
         first_name_txt.getBackground().setAlpha(75);
-        TextView last_name_txt = findViewById(R.id.lastname_textview);
+        last_name_txt = findViewById(R.id.lastname_textview);
         last_name_txt.getBackground().setAlpha(75);
-        TextView email_txt = findViewById(R.id.email_textview);
+        email_txt = findViewById(R.id.email_textview);
         email_txt.getBackground().setAlpha(75);
-        TextView country_txt = findViewById(R.id.country_textview);
+        country_txt = findViewById(R.id.country_textview);
         country_txt.getBackground().setAlpha(75);
-        TextView dob_txt = findViewById(R.id.dob_textview);
+        dob_txt = findViewById(R.id.dob_textview);
         dob_txt.getBackground().setAlpha(75);
-        TextView password_txt = findViewById(R.id.password_textview);
+        password_txt = findViewById(R.id.password_textview);
         password_txt.getBackground().setAlpha(75);
     }
 }
