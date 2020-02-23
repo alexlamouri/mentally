ca78b91dbe398e484134708bff1464d98acc65ce
Fareeha Fatima <fareehaf@outlook.com>
2020-02-22 19:54:25 -0500

    [PAC-33] Add a new activity for habit tracker module

---
+++ MentAlly/app/src/main/java/com/pacman/MentAlly/HabitTrackerActivity.java
@@ -0,0 +1,14 @@
+package com.pacman.MentAlly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HabitTrackerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_habit_tracker);
+    }
+}
