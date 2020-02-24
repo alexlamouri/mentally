67155db273327cc9c6e07005d0b095d1664e70ea
tangja <47670756+tangja@users.noreply.github.com>
2020-02-24 00:52:39 -0500

    [PAC-25] Add backgrounds based on time of day to home page

--- MentAlly/app/src/main/java/com/pacman/MentAlly/ui/menu/HomeFragment.java
+++ MentAlly/app/src/main/java/com/pacman/MentAlly/ui/menu/HomeFragment.java
@@ -11,10 +11,29 @@ import androidx.fragment.app.Fragment;
 
 import com.pacman.MentAlly.R;
 
+import java.util.Calendar;
+
 public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.frag_home_day, container, false);
+        Calendar calendar = Calendar.getInstance();
+        int time = calendar.get(Calendar.HOUR_OF_DAY);
+        View root;
+
+        //set background based on time of day
+        if(time >= 5 && time < 11){
+            root = inflater.inflate(R.layout.frag_home_dawn, container, false);
+        }
+        else if(time >= 11 && time < 18){
+            root = inflater.inflate(R.layout.frag_home_day, container, false);
+        }
+        else if(time >= 18 && time < 22){
+            root = inflater.inflate(R.layout.frag_home_sunset, container, false);
+        }else{
+            root = inflater.inflate(R.layout.frag_home_night, container, false);
+        }
+
+        return root;
     }
 }
