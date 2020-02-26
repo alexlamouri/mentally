94d776898b00d0679c96f2c75043d6ebc7d1c14f
NavyaGupta <navya20gupta@gmail.com>
2020-02-26 00:33:40 -0500

    [PAC-28] Created the first page of PTSD questionnaire

---
+++ MentAlly/app/src/main/java/com/pacman/MentAlly/ui/PTSD/PTSDResults.java
@@ -0,0 +1,19 @@
+package com.pacman.MentAlly.ui.PTSD;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.content.Intent;
+
+import com.pacman.MentAlly.R;
+public class PTSDResults extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ptsdresults);
+    }
+}
