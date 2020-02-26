dd5919364bfb2df2419d31ee72307d98259f2416
NavyaGupta <navya20gupta@gmail.com>
2020-02-26 14:28:15 -0500

    [PAC-28] Completed my task of PTSD questionnaire

--- MentAlly/app/src/main/java/com/pacman/MentAlly/ui/PTSD/PTSDActivity.java
+++ MentAlly/app/src/main/java/com/pacman/MentAlly/ui/PTSD/PTSDActivity.java
@@ -10,11 +10,63 @@ import android.content.Intent;
 
 import com.pacman.MentAlly.R;
 
+import org.w3c.dom.Text;
+
 public class PTSDActivity extends AppCompatActivity {
+
+    private PTSDQuestions mQues = new PTSDQuestions();
+    private TextView mQuesView;
+    private int mQuesNum=0;
+    private int mScore=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ptsd2);
+
+        mQuesView = (TextView) findViewById(R.id.quesP);
+        Button yesB = (Button) findViewById(R.id.yesP);
+        Button noB = (Button) findViewById(R.id.noP);
+
+        updateQuestion();
+
+        yesB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mScore = mScore + 1;
+                if (mQuesNum == mQues.getLength()) {
+                    updateResult();
+                } else {
+                    updateQuestion();
+                }
+            }
+        });
+
+        noB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mQuesNum == mQues.getLength()){
+                    updateResult();
+                } else {
+                    updateQuestion();
+                }
+            }
+        });
+
+
+    }
+
+    private void updateQuestion(){
+        mQuesView.setText(mQues.getQuestion(mQuesNum));
+        mQuesNum++;
+    }
+
+    private void updateResult(){
+        Intent i = new Intent(getApplicationContext(),PTSDResults.class);
+        Bundle b = new Bundle();
+        b.putInt("score",mScore);
+        i.putExtras(b);
+        PTSDActivity.this.finish();
+        startActivity(i);
     }
 }
--- MentAlly/app/src/main/java/com/pacman/MentAlly/ui/PTSD/PTSDResults.java
+++ MentAlly/app/src/main/java/com/pacman/MentAlly/ui/PTSD/PTSDResults.java
@@ -9,11 +9,37 @@ import android.widget.TextView;
 import android.content.Intent;
 
 import com.pacman.MentAlly.R;
+
+import org.w3c.dom.Text;
+
 public class PTSDResults extends AppCompatActivity {
+
+    private TextView mResults;
+    private Button mRetry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ptsdresults);
+        setContentView(R.layout.activity_ptsdresults2);
+
+        mResults = (TextView) findViewById(R.id.resultP);
+        mRetry = (Button) findViewById(R.id.retryP);
+
+        Bundle b = getIntent().getExtras();
+        int score = b.getInt("score");
+
+        if (score >= 3){
+            mResults.setText("You are likely to be experiencing PTSD");
+        } else {
+            mResults.setText("You are not likely to be experiencing PTSD");
+        }
+
+        mRetry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),PTSD.class);
+                startActivity(i);
+            }
+        });
     }
 }
