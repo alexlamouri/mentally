9e159322764635a501c2eb60ff3e7ac261f3d4ac
Viju Hiremath <Viju@Viju.local>
2020-02-26 17:44:53 -0500

    need to switch branches

--- MentAlly/app/src/main/java/com/pacman/MentAlly/ui/breathing/BreathingActivity.java
+++ MentAlly/app/src/main/java/com/pacman/MentAlly/ui/breathing/BreathingActivity.java
@@ -1,4 +1,74 @@
 package com.pacman.MentAlly.ui.breathing;
 
-public class BreathingActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.os.Bundle;
+import com.pacman.MentAlly.R;
+import android.content.Intent;
+
+
+public class BreathingActivity extends AppCompatActivity {
+
+    private Button startButton;
+    private Spinner inhale;
+    private Spinner hold;
+    private Spinner exhale;
+    private Spinner breaths;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_breathing);
+
+        //inhale
+        inhale = findViewById(R.id.inhale);
+        String[] inhaleList = new String[]{"2", "3", "4", "5", "6", "7", "8", "9", "10"};
+        ArrayAdapter<String> inhaleAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, inhaleList);
+        inhale.setAdapter(inhaleAdapter);
+        
+        //hold
+        hold = findViewById(R.id.hold);
+        String[] holdList = new String[]{"2", "3", "4", "5", "6", "7", "8", "9", "10"};
+        ArrayAdapter<String> holdAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, holdList);
+        hold.setAdapter(holdAdapter);
+
+        //exhale
+        exhale = findViewById(R.id.exhale);
+        String[] exhaleList = new String[]{"2", "3", "4", "5", "6", "7", "8", "9", "10"};
+        ArrayAdapter<String> exhaleAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, exhaleList);
+        exhale.setAdapter(exhaleAdapter);
+
+        //breaths
+        breaths = findViewById(R.id.breaths);
+        String[] breathsList = new String[]{"2", "3", "4", "5", "6", "7", "8", "9", "10"};
+        ArrayAdapter<String> breathsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, breathsList);
+        breaths.setAdapter(breathsAdapter);
+
+        startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                openBreathingExerciseActivity();
+            }
+
+
+        });
+
+
+    }
+
+    public void openBreathingExerciseActivity() {
+        Intent intent = new Intent(this, BreathingExerciseActivity.class);
+        intent.putExtra("inhale", Integer.parseInt(inhale.getSelectedItem().toString()));
+        intent.putExtra("hold", Integer.parseInt(hold.getSelectedItem().toString()));
+        intent.putExtra("exhale", Integer.parseInt(exhale.getSelectedItem().toString()));
+        intent.putExtra("breaths", Integer.parseInt(breaths.getSelectedItem().toString()));
+        startActivity(intent);
+    }
+
 }
