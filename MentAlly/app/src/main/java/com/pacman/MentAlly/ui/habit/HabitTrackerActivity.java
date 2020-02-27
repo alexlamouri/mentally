6a519dcb926bd1deb1e3099da849fb7db5197f13
Fareeha Fatima <fareehaf@outlook.com>
2020-02-27 00:46:36 -0500

    [PAC-34] Completed layout view of new habit screen

--- MentAlly/app/src/main/java/com/pacman/MentAlly/ui/habit/HabitTrackerActivity.java
+++ MentAlly/app/src/main/java/com/pacman/MentAlly/ui/habit/HabitTrackerActivity.java
@@ -4,16 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.pacman.MentAlly.R;
 
 public class HabitTrackerActivity extends AppCompatActivity {
+
+    private FloatingActionButton newHabit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_habit_tracker);
+
+        newHabit = findViewById(R.id.new_habit);
+        newHabit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
 
         initHabitList();
     }
