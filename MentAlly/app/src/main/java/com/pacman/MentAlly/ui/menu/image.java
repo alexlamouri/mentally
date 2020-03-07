1ef6ebab3a381cb095b563900fe7de95b54cd4dd
Malaika Zaidi <malaika.zaidi@mail.utoronto.ca>
2020-03-06 21:14:16 -0500

    [PAC-41] Completed theme customization view

---
+++ MentAlly/app/src/main/java/com/pacman/MentAlly/ui/menu/image.java
@@ -0,0 +1,17 @@
+package com.pacman.MentAlly.ui.menu;
+
+
+public class image {
+    int image;
+    public image(int image){
+        this.image=image;
+
+    }
+    public int getImage(){
+        return  image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
