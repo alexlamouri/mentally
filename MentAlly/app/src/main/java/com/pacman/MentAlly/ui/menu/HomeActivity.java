1b281badb5c0764057048b1ad21719c996ca08d5
tangja <47670756+tangja@users.noreply.github.com>
2020-03-12 04:36:57 -0400

    [PAC-38] Create a new activity class for the home page

--- MentAlly/app/src/main/java/com/pacman/MentAlly/ui/menu/HomeActivity.java
+++ MentAlly/app/src/main/java/com/pacman/MentAlly/ui/menu/HomeActivity.java
@@ -1,9 +1,11 @@
 package com.pacman.MentAlly.ui.menu;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,27 +15,27 @@ import com.pacman.MentAlly.R;
 
 import java.util.Calendar;
 
-public class HomeFragment extends Fragment {
-    @Nullable
+public class HomeActivity extends MainActivity {
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FrameLayout contentFrameLayout = findViewById(R.id.frag_container);
+
         Calendar calendar = Calendar.getInstance();
         int time = calendar.get(Calendar.HOUR_OF_DAY);
-        View root;
 
         //set background based on time of day
         if(time >= 5 && time < 11){
-            root = inflater.inflate(R.layout.frag_home_dawn, container, false);
+            getLayoutInflater().inflate(R.layout.frag_home_dawn, contentFrameLayout);
         }
         else if(time >= 11 && time < 18){
-            root = inflater.inflate(R.layout.frag_home_day, container, false);
+            getLayoutInflater().inflate(R.layout.frag_home_day, contentFrameLayout);
         }
         else if(time >= 18 && time < 22){
-            root = inflater.inflate(R.layout.frag_home_sunset, container, false);
+            getLayoutInflater().inflate(R.layout.frag_home_sunset, contentFrameLayout);
         }else {
-            root = inflater.inflate(R.layout.frag_home_night, container, false);
+            getLayoutInflater().inflate(R.layout.frag_home_night, contentFrameLayout);
         }
-
-        return root;
     }
 }
