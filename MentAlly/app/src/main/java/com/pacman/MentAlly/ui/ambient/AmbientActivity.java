fe0c0b3278c027adb59b0a29e28635dbedc1daa6
Fareeha Fatima <fareehaf@outlook.com>
2020-03-13 17:30:47 -0400

    [PAC-44] Build service to play audio and connect to activity

--- MentAlly/app/src/main/java/com/pacman/MentAlly/ui/ambient/AmbientActivity.java
+++ MentAlly/app/src/main/java/com/pacman/MentAlly/ui/ambient/AmbientActivity.java
@@ -1,16 +1,75 @@
 package com.pacman.MentAlly.ui.ambient;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.PersistableBundle;
+import android.widget.Toast;
 
 import com.pacman.MentAlly.R;
 
 public class AmbientActivity extends AppCompatActivity {
 
+    private MediaPlaybackService audioService;
+    private boolean serviceBound = false;
+    private ServiceConnection serviceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            MediaPlaybackService.LocalBinder binder = (MediaPlaybackService.LocalBinder) service;
+            audioService = binder.getService();
+            serviceBound = true;
+
+            Toast.makeText(AmbientActivity.this, "Service Bound", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            serviceBound = false;
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ambient);
+        playAudio("/raw/audio1");
+    }
+
+    private void playAudio(String media) {
+        if (!serviceBound) {
+            Intent playerIntent = new Intent(this, MediaPlaybackService.class);
+            playerIntent.putExtra("media", media);
+            startService(playerIntent);
+            bindService(playerIntent, serviceConnection, Context.BIND_AUTO_CREATE);
+        } else {
+
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putBoolean("ServiceState", serviceBound);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        serviceBound = savedInstanceState.getBoolean("ServiceState");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (serviceBound) {
+            unbindService(serviceConnection);
+            audioService.stopSelf();
+        }
     }
 }
