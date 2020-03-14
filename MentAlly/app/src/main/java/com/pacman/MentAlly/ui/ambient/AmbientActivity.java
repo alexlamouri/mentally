de28b5e9e5a102e954b3c829c3e5415ab07cedc6
Fareeha Fatima <fareehaf@outlook.com>
2020-03-13 21:48:03 -0400

    [PAC-43] Completed view for list and inidividual audio files. Added code to display audio choices

--- MentAlly/app/src/main/java/com/pacman/MentAlly/ui/ambient/AmbientActivity.java
+++ MentAlly/app/src/main/java/com/pacman/MentAlly/ui/ambient/AmbientActivity.java
@@ -1,16 +1,64 @@
 package com.pacman.MentAlly.ui.ambient;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContentResolverCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 
 import com.pacman.MentAlly.R;
 
+import java.util.ArrayList;
+
 public class AmbientActivity extends AppCompatActivity {
+
+    private static final String TAG = "Ambient Activity";
+    private ArrayList<AudioFile> audioList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ambient);
+
+//        AudioFile test = new AudioFile("beethoven's ...", 3);
+//        audioList.add(test);
+        loadAudio();
+        Log.d(TAG, "onCreate: about to init recycler");
+        initRecyclerView();
+    }
+
+    private void loadAudio() {
+        ContentResolver contentResolver = getContentResolver();
+
+        Uri uri = MediaStore.Audio.Media.INTERNAL_CONTENT_URI;
+        String selection = MediaStore.Audio.Media.IS_MUSIC + "!= 0";
+        String sortOrder = MediaStore.Audio.Media.TITLE + " ASC";
+        Cursor cursor = contentResolver.query(uri, null, selection, null, sortOrder);
+
+        if (cursor != null && cursor.getCount() > 0) {
+            audioList = new ArrayList<>();
+            while (cursor.moveToNext()) {
+                String data = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA));
+                String title = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE));
+                String duration = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DURATION));
+                audioList.add(new AudioFile(data, title, duration));
+            }
+        }
+        cursor.close();
+    }
+
+    private void initRecyclerView() {
+        Log.d(TAG, "Initialize recycler view");
+        RecyclerView audioListView = findViewById(R.id.audioList);
+        AudioAdapter adapter = new AudioAdapter(this, audioList);
+        audioListView.setAdapter(adapter);
+        audioListView.setLayoutManager(new LinearLayoutManager(this));
+        adapter.notifyDataSetChanged();
     }
 }
---
+++ MentAlly/app/src/main/java/com/pacman/MentAlly/ui/ambient/AudioFile.java
@@ -0,0 +1,38 @@
+package com.pacman.MentAlly.ui.ambient;
+
+public class AudioFile {
+
+    private String data;
+    private String title;
+    private String length;
+
+    public AudioFile(String data, String audioTitle, String audioLength) {
+        this.data = data;
+        this.title = audioTitle;
+        this.length = audioLength;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAudioLength(String audioLength) {
+        this.length = audioLength;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAudioLength() {
+        return length;
+    }
+
+    public String getData() {
+        return data;
+    }
+}
