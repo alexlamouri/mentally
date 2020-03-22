289d2295646a1059515a46e1d16aa69f1b81f97f
Kavya Mehta <kavyamehta@Kavyas-MacBook-Pro.local>
2020-03-21 20:27:32 -0400

    PAC-45: Fixed bugs, and added profile

--- MentAlly/app/src/main/java/com/pacman/MentAlly/ui/profile/ProfileActivity.java
+++ MentAlly/app/src/main/java/com/pacman/MentAlly/ui/profile/ProfileActivity.java
@@ -2,6 +2,7 @@ package com.pacman.MentAlly.ui.profile;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -19,31 +21,26 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.pacman.MentAlly.R;
+import com.pacman.MentAlly.ui.home.MainActivity;
 
-public class ProfileFragment extends Fragment {
+public class ProfileActivity extends MainActivity {
     private Button profile_btn;
     private ImageButton first_name_btn;
-    private ImageButton last_name_btn;
-    private ImageButton email_btn;
     private ImageButton dob_btn;
     private ImageButton country_btn;
-    private ImageButton password_btn;
 
     private TextView first_name_txt;
-    private TextView last_name_txt;
-    private TextView email_txt;
     private TextView country_txt;
     private TextView dob_txt;
-    private TextView password_txt;
     private FirebaseUser user;
 
     public FirebaseFirestore myDatabase;
 
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.activity_profile, container, false);
-
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        FrameLayout contentFrameLayout = findViewById(R.id.frag_container);
+        getLayoutInflater().inflate(R.layout.activity_profile, contentFrameLayout);
 
 //        first_name_btn = root.findViewById(R.id.edit_first_name);
 //        first_name_btn.getBackground().setAlpha(65);
@@ -53,11 +50,11 @@ public class ProfileFragment extends Fragment {
 //        country_btn.getBackground().setAlpha(65);
 
         //set all textview transparency level
-        first_name_txt = root.findViewById(R.id.firstname_textview);
+        first_name_txt = findViewById(R.id.firstname_textview);
         first_name_txt.getBackground().setAlpha(75);
-        country_txt = root.findViewById(R.id.country_textview);
+        country_txt = findViewById(R.id.country_textview);
         country_txt.getBackground().setAlpha(75);
-        dob_txt = root.findViewById(R.id.dob_textview);
+        dob_txt = findViewById(R.id.dob_textview);
         dob_txt.getBackground().setAlpha(75);
 
         myDatabase = FirebaseFirestore.getInstance();
@@ -81,7 +78,6 @@ public class ProfileFragment extends Fragment {
                 }
             });
         }
-        return root;
     }
 
 }
