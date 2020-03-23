88b15fe8e340f5a8163a13865ea300981003d1a9
Malaika Zaidi <malaika.zaidi@mail.utoronto.ca>
2020-03-23 18:13:06 -0400

    [PAC-53] Added a splash screen to the app

--- MentAlly/app/src/main/java/com/pacman/MentAlly/ui/login/LoginActivity.java
+++ MentAlly/app/src/main/java/com/pacman/MentAlly/ui/login/LoginActivity.java
@@ -14,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
