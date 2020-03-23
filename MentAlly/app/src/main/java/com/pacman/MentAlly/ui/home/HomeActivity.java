fb85d896a7372a7abf3b22076c91efe09c367bdb
Malaika Zaidi <malaika.zaidi@mail.utoronto.ca>
2020-03-23 14:42:31 -0400

    [PAC-47] Enhanced mood tracker view and wallpaper

--- MentAlly/app/src/main/java/com/pacman/MentAlly/ui/home/HomeActivity.java
+++ MentAlly/app/src/main/java/com/pacman/MentAlly/ui/home/HomeActivity.java
@@ -1,8 +1,15 @@
 package com.pacman.MentAlly.ui.home;
 
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 
+import androidx.annotation.ColorInt;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatImageView;
 
@@ -11,20 +18,33 @@ import com.pacman.MentAlly.ui.menu.WallpaperFragment;
 
 import java.util.Calendar;
 
+import pl.droidsonroids.gif.GifImageView;
+
 public class HomeActivity extends MainActivity {
     private int theme;
+    Button d;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FrameLayout contentFrameLayout = findViewById(R.id.frag_container);
+        final FrameLayout contentFrameLayout = findViewById(R.id.frag_container);
+        d = findViewById(R.id.def);
 
         WallpaperFragment w = new WallpaperFragment();
         this.theme = w.theme;
-        getLayoutInflater().inflate(R.layout.wallpaperlayout, contentFrameLayout);
+        System.out.println(this.theme);
         if (this.theme != -1) {
+            getLayoutInflater().inflate(R.layout.wallpaperlayout, contentFrameLayout);
             AppCompatImageView view = findViewById(R.id.imageView);
             view.setImageResource(this.theme);
         }
+        else{
+            getLayoutInflater().inflate(R.layout.defaultwallpaperlayout, contentFrameLayout);
+
+        }
+
+
+
     }
 }
